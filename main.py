2b5a87821972323bc8d2cc8e7249fda7c8210727
gabas82 <gabas82@gmail.com>
2025-12-20 23:01:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,27 +7,33 @@ app = Flask(__name__)
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
+
 @app.route("/", methods=["GET"])
 def home():
     return "Bot is running"
 
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-   text = (
-    f"🟢 SIGNAL\n"
-    f"PAIR: {data.get('symbol')}\n"
-    f"PRICE: {data.get('price')}\n"
-    f"TF: {data.get('timeframe')}\n"
-    f"SIDE: {data.get('side')}"
-) 
+
+    text = (
+        f"🟢 SIGNAL\n"
+        f"PAIR: {data.get('symbol')}\n"
+        f"PRICE: {data.get('price')}\n"
+        f"TF: {data.get('timeframe')}\n"
+        f"SIDE: {data.get('side')}"
+    )
+
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {
         "chat_id": TELEGRAM_CHAT_ID,
         "text": text
     }
+
     requests.post(url, json=payload)
     return {"status": "ok"}
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000) 
+    app.run(host="0.0.0.0", port=10000)
